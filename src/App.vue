4242bbbbf6b193e16c9bc5fa4d1f531b5aa47eca
wambuinjoroge <christinenjoroge93@gmail.com>
2018-08-21 12:49:53 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>Hello World,"Server is: {{status}}"</h1>
+</template>
+
+<script>
+// a component is a reusable piece of our instance,basically extends the vue instance
+    export default {
+       data:function () {
+            return{
+                status:"Stable"
+            }
+        },
+
+    }
+
+</script>
+
+<style>
+
+</style>
